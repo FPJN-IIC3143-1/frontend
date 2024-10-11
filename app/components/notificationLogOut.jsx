567a15d00e7ec263f1ad6bc98052e22d44ca3213
PipeOlivares <felipe.olivares@uc.cl>
2024-10-11 15:39:37 -0300

    Notification/LogOut Bar walking skelleton: simple routing, visible in homepage.

--- app/components/notificationLogOut.jsx
+++ app/components/notificationLogOut.jsx
@@ -1,20 +1,41 @@
+import { useNavigate } from "@remix-run/react"
 
 export default function NotificationLogOut() {
 
+  const navigate = useNavigate()
+
+  const handleNotification = () => {
+    console.log("Notification")
+  }
+
+  const handleLogOut = () => {
+    // Log out user
+    console.log("LogOut")
+    navigate("/")
+  }
+
   return(
-    <div className="container bg-[#182F40] flex flex-row justify-center items-center w-[310px] h-[64px] "
-      style={{ width: 310, height: 64, backgroundColor: "#182F40" }}  
-    >
-      <div className="vertLine w-[5px] h-[50px] border-l-white border-l-solid border-l-4"/>
-      <img src="../../public/NotificationLogOut/notifications.png" 
-        alt="notification" 
-        className="w-[50px] h-[50px] pt-[7px] pb-[7px]"
-      />
-      <img src="../../public/NotificationLogOut/logout.png" 
-        alt="logOut" 
-        className="w-[50px] h-[50px] pt-[7px] pb-[7px]"
-      />
-      <div className="vertLine w-[5px] h-[50px] border-r-white border-r-solid border-r-4"/>
+    <div className="container bg-[#182F40] flex flex-row justify-center items-center 
+        w-[280px] h-[50px] rounded-[2px] gap-12 px-8 absolute top-0 right-0 m-1">
+      <div className="vertLine w-[0px] h-[25px] bg-gray-100 border-x-gray-100 border-x-solid rounded-[2px] border-x-2 border-"/>
+      <button className="flex justify-center items-center w-[40px] h=[50px] rounded-[8px] hover:border-white hover:border hover:border-solid border-none" 
+        onClick={handleNotification}
+      >  
+        <img src="/NotificationLogOut/notifications.png" 
+          alt="notification" 
+          className="w-[30px] h-[40px] pt-[7px] pb-[7px]"
+        />
+      </button>
+      <button className="LogOutButton flex justify-center items-center w-[40px] h=[50px] rounded-[8px] hover:border-white hover:border hover:border-solid border-none" 
+        onClick={handleLogOut}
+      >
+        <img src="/NotificationLogOut/logout.png" 
+          alt="logOut" 
+          className="w-[30px] h-[40px] pt-[7px] pb-[7px]"
+        />   
+      </button> 
+      <div className="vertLine w-[0px] h-[25px] bg-gray-100 border-x-gray-100 border-x-solid rounded-[2px] border-x-2 border-"/>
+
     </div>
   )
 }
